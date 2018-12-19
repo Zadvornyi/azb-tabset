6b2112fad4e232fc0ebf701a071fe843275c6de7
Andrii Zadvornyi <andrii.zadvornyi@globallogic.com>
2018-12-19 15:08:59 +0200

    fix bug(azb-tabset) add export file for global module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
-import { AzbTab, AzbTabTitle, AzbTabContent } from './azb-tabset/azb-tab';
 import { AzbTabSetModule } from './azb-tabset/azb-tabset.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    AzbTab,
-    AzbTabTitle,
-    AzbTabContent,
   ],
   imports: [
     BrowserModule,
--- src/app/azb-tabset/azb-tabset.module.ts
+++ src/app/azb-tabset/azb-tabset.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AzbTabSetComponent } from './azb-tabset.component';
-
+import { AzbTab, AzbTabTitle, AzbTabContent } from './azb-tab';
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AzbTabSetComponent],
-  exports: [AzbTabSetComponent]
+  declarations: [
+    AzbTab,
+    AzbTabTitle,
+    AzbTabContent,
+    AzbTabSetComponent
+  ],
+  exports: [
+    AzbTab,
+    AzbTabTitle,
+    AzbTabContent,
+    AzbTabSetComponent
+  ]
 })
 export class AzbTabSetModule {
   
